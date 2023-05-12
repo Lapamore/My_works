7079d6095a1517a1cbc24c5dcedbcf5e27518e88
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2023-05-12 17:29:50 +0300

    Update smart_test.R

--- Fundamentals of statistics. Part 2/smart_test.R
+++ Fundamentals of statistics. Part 2/smart_test.R
@@ -2,9 +2,9 @@ smart_test <- function(df){
   df <- table(df)
   if (min(df) > 5){
     test <- chisq.test(df)       
-    print(c(test$statistic,test$parameter,test$p.value))
+    return(c(test$statistic,test$parameter,test$p.value))
   }else{
     test <- fisher.test(df)
-    print(test$p.value)
+    return(test$p.value)
   }
 }
