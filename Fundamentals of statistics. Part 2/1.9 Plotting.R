e4350053f96db2cdd596ccd456d2ef30696f6d77
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2023-05-13 21:30:07 +0300

    Update 1.9 Plotting.R

--- Fundamentals of statistics. Part 2/1.9 Plotting.R
+++ Fundamentals of statistics. Part 2/1.9 Plotting.R
@@ -1,3 +1,6 @@
+#Используя библиотеку ggplot2 и встроенные данные diamonds, постройте график распределения частот переменной color, на котором за цвет заполнения столбиков отвечает переменная cut. 
+#Сохраните код графика в переменную obj. В итоге должен получиться вот такой график.
+
 library(ggplot2)
 ggplot(data = diamonds, aes(x = color,fill = cut, position = "dodge"))+
   geom_histogram(stat = "count", position = position_dodge())
