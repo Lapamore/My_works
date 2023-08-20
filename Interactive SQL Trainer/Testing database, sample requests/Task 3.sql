b38668a49d6f41cdc0d21c56156fbbb86f61523b
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2023-08-20 18:50:38 +0600

    Create Task 3.sql

---
+++ Interactive SQL Trainer/Testing database, sample requests/Task 3.sql
@@ -0,0 +1,10 @@
+/*
+Вывести студентов (различных студентов), имеющих максимальные результаты попыток . Информацию отсортировать в алфавитном порядке по фамилии студента.
+*/
+
+SELECT name_student,  
+      result
+FROM student
+INNER JOIN attempt USING (student_id)
+WHERE attempt.result = (SELECT MAX(result) FROM attempt)  
+ORDER BY name_student;
