81654c7c5b4b906329ca0529b240fb5fd7410faf
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2023-08-20 19:29:44 +0600

    Create Task 2.sql

---
+++ Interactive SQL Trainer/Database "Online book store"/Task 2.sql
@@ -0,0 +1,8 @@
+/*
+Удалить из таблиц book и supplyкниги, цены которых заканчиваются на 99 копеек. Например, книга с ценой 670.99 должна быть удалена.
+*/
+
+DELETE FROM book
+WHERE price LIKE "%.99";
+DELETE FROM supply
+WHERE price LIKE "%.99";
